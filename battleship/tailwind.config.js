cd7601aa756ba0fb69e2195a08c0b5b04df52fc2
Sebastian Torres O <storresoliv@gmail.com>
2022-05-04 21:18:12 -0400

    feat: mobile first

--- battleship/tailwind.config.js
+++ battleship/tailwind.config.js
@@ -1,9 +1,16 @@
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}"
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
+    screens: {
+      sxx: "280px",
+      sx: "360px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
